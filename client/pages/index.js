7ad2273ea595971bb86f7938cf40926453a01ba7
kokdemirdev <kokdemirdev@gmail.com>
2022-04-25 12:49:10 +0300

    init load

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,25 +9,30 @@ const TILE_HEIGHT = 64
 //Utils
 
 //Game Mechanics
-const Player = {
-  x: 0,
-  y: 0,
-  dirX: 0,
-  dirY: 0,
-  health: 100,
-  coins: 0,
-  bullets: 0,
-  medkits: 0,
-  update: () => {
-  },
-  draw: () => {
+class Player {
+  x = 0
+  y = 0
+  dirX = 0
+  dirY = 0
+  health = 100
+  coins = 0
+  bullets = 0
+  medkits = 0
+  update = () => {
+  }
+  draw = () => {
   }
 }
 
-const Game = {
-  users: [],
-  images: {},
-  layers: [
+class Game {
+  constructor(ctx) {
+    this.ctx = ctx
+    this.init()
+  }
+
+  users = []
+  images = {}
+  layers = [
     [
       1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
       1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -38,27 +43,24 @@ const Game = {
       1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
       1, 1, 1, 1, 1, 1, 1, 1, 1, 1
     ]
-  ],
-  load: async () => {
+  ]
+  init = async () => {
+    console.log('init')
+    const tile0 = await this.loadImage('0', './assets/layers/0.png')
+    const tile1 = await this.loadImage('0', './assets/layers/1.png')
 
-    let [tile1, tile2] = await Promise.all([
-      this.loadImage('1', '../public/assets/layers/0.png'),
-      this.loadImage('2', '../public/assets/layers/1.png')
-    ])
-
-    // @ts-ignore
     this.images = {
-      1: tile1,
-      2: tile2
+      0: tile0,
+      1: tile1
     }
-  },
-  loadImage: (key, src) => {
+    console.log(this.images)
+  }
+  loadImage = (key, src) => {
     const img = new Image()
     const d = new Promise((_resolve, reject) => {
       img.onload = function () {
-        // @ts-ignore
-        this.images[key] = img
-      }.bind(this)
+        _resolve(img)
+      }
 
       img.onerror = function () {
         reject(`Could not load image: ${src}`)
@@ -67,13 +69,19 @@ const Game = {
 
     img.src = src
     return d
-  },
-  init: () => {
-  },
-  update: () => {
-  },
-  draw: () => {
-  },
+  }
+  update = () => {
+    console.log('update')
+  }
+  draw = () => {
+    console.log('draw')
+    console.log(this.images)
+    //clear canvas
+    this.ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+    //draw tiles
+    this.ctx.drawImage(this.images[0], 0, 0, TILE_WIDTH, TILE_HEIGHT, 100, 100)
+  }
 }
 
 class GamePage extends Component {
@@ -81,15 +89,37 @@ class GamePage extends Component {
     super(props);
 
     this.canvasRef = React.createRef()
+    this.lastLoop = null
 
     this.state = {
-      CURRENT_STEP: 0
+      CURRENT_STEP: 0,
+      isGameRunning: false
     }
   }
 
-  canvasRef
+  start = async () => {
+    if (!this.state.isGameRunning) {
+      this.game = new Game(this.getCtx())
+      await this.game.init()
+      this.loop()
+    }
+    this.setState(state => ({isGameRunning: !state.isGameRunning}))
+  }
 
-  // @ts-ignore
+  loop = () => {
+    requestAnimationFrame(() => {
+      const now = Date.now()
+      if (now - this.lastLoop > (1000 / 30)) {
+        this.game.update()
+        this.lastLoop = Date.now()
+      }
+      this.game.draw()
+      if (this.state.isGameRunning) {
+        this.loop()
+      }
+    })
+  }
+
   getCtx = () => this.canvasRef.current.getContext('2d')
 
   render() {
@@ -97,6 +127,7 @@ class GamePage extends Component {
       <>
         <div>
           <h1 style={{textAlign: "center"}}>Battle Royale Multiplayer Game</h1>
+          <button onClick={this.start}>Start</button>
           <div style={{
             height: "100%",
             display: "flex",
@@ -117,3 +148,5 @@ class GamePage extends Component {
 }
 
 export default GamePage
+
+
